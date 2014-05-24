2cdc3dfe0a712c2e83badde018ef6fed9efe37f8
Marc Rene Arns <arns@stridor.net>
2014-05-24 14:51:19 +0200

    add doc to the root

---
+++ doc.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2014 Marc René Arns. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+/*
+Package wrapcontrib provides middleware (wraps) and helpers for github.com/go-on/wrap.
+
+Status
+
+100% test coverage.
+This package is considered complete and the API is stable.
+
+More Middleware
+
+More (WIP,API may change) middleware can be found at github.com/go-on/wrap-contrib-testing
+
+*/
+package wrapcontrib
