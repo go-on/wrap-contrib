e414810ac464275da1601d5ba66da18e1a71bd56
Marc Rene Arns <arns@stridor.net>
2014-07-16 13:14:24 +0200

    add PrepareLikeMux to prepare a request like ServeMux from net/http

---
+++ wraps/prepare.go
@@ -0,0 +1,57 @@
+package wraps
+
+import (
+	"net/http"
+	"path"
+
+	"github.com/go-on/wrap"
+)
+
+// this is taken from pkg/net
+
+// Return the canonical path for p, eliminating . and .. elements.
+func cleanPath(p string) string {
+	if p == "" {
+		return "/"
+	}
+	if p[0] != '/' {
+		p = "/" + p
+	}
+	np := path.Clean(p)
+	// path.Clean removes trailing slash except for root;
+	// put the trailing slash back if necessary.
+	if p[len(p)-1] == '/' && np != "/" {
+		np += "/"
+	}
+	return np
+}
+
+type prepareLikeMux struct{}
+
+// Prepare prepareLikeMuxs a request the same way that net/http ServeMux does
+func PrepareLikeMux() wrap.Wrapper {
+	return prepareLikeMux{}
+}
+
+func (p prepareLikeMux) Wrap(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, rq *http.Request) {
+		if rq.RequestURI == "*" {
+			if rq.ProtoAtLeast(1, 1) {
+				w.Header().Set("Connection", "close")
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// taken from net/http ServeMux and modified
+		if rq.Method != "CONNECT" {
+			if p := cleanPath(rq.URL.Path); p != rq.URL.Path {
+				url := *rq.URL
+				url.Path = p
+				http.RedirectHandler(url.String(), http.StatusMovedPermanently).ServeHTTP(w, rq)
+				return
+			}
+		}
+		next.ServeHTTP(w, rq)
+	})
+}
