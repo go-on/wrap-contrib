450f14212310a9557c450bef63404b8d51826ac0
Marc Rene Arns <arns@stridor.net>
2014-07-30 14:12:21 +0200

    add Defer and DeferFunc

---
+++ wraps/defer.go
@@ -0,0 +1,28 @@
+package wraps
+
+import (
+	"net/http"
+
+	"github.com/go-on/wrap"
+)
+
+type (
+	// defer something after the request handling
+	defer_        struct{ http.Handler }
+	deferWrapper_ struct{ wrap.Wrapper }
+)
+
+func (ø *defer_) Wrap(in http.Handler) (out http.Handler) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() { ø.ServeHTTP(w, r) }()
+		in.ServeHTTP(w, r)
+	})
+}
+
+func DeferFunc(fn func(http.ResponseWriter, *http.Request)) wrap.Wrapper {
+	return &defer_{http.HandlerFunc(fn)}
+}
+
+func Defer(h http.Handler) wrap.Wrapper {
+	return &defer_{h}
+}
---
+++ wraps/defer_test.go
@@ -0,0 +1,41 @@
+package wraps
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/go-on/wrap"
+	"github.com/go-on/wrap-contrib/helper"
+)
+
+func anyway(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`anyway`))
+}
+
+func TestDeferFunc(t *testing.T) {
+	r := wrap.New(
+		DeferFunc(anyway),
+		wrap.Handler(panicker{}),
+	)
+	rw, req := helper.NewTestRequest("GET", "/")
+	defer func() { recover() }()
+	r.ServeHTTP(rw, req)
+	err := helper.AssertResponse(rw, "anyway", 200)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestDefer(t *testing.T) {
+	r := wrap.New(
+		Defer(http.HandlerFunc(anyway)),
+		wrap.Handler(panicker{}),
+	)
+	rw, req := helper.NewTestRequest("GET", "/")
+	defer func() { recover() }()
+	r.ServeHTTP(rw, req)
+	err := helper.AssertResponse(rw, "anyway", 200)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
