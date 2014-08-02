db3a574de475cb049bedb326eab0d2fc19dcf998
Marc Rene Arns <arns@stridor.net>
2014-08-02 15:34:03 +0200

    fix helper doc

---
+++ helper/doc.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2014 Marc René Arns. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+/*
+Package helper provides helpers for the development and testing of middleware wrappers.
+
+
+*/
+package helper
