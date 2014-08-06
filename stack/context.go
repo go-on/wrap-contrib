c1360a9bb4a5428991d6f1288583c1278e21f5c7
Marc Rene Arns <arns@stridor.net>
2014-08-06 16:31:48 +0200

    add stack for simple middleware apps with one top level context

---
+++ stack/context.go
@@ -0,0 +1,50 @@
+package stack
+
+import (
+	"net/http"
+
+	"github.com/go-on/wrap"
+)
+
+// basicContext is a simple basic context that supports the setting and getting of errors
+type basicContext struct {
+	http.ResponseWriter
+	err error
+}
+
+// make sure to fulfill the ContextInjecter interface
+var _ wrap.ContextInjecter = &basicContext{}
+var _ = wrap.ValidateContextInjecter(&basicContext{})
+
+func (c *basicContext) Context(ctxPtr interface{}) (found bool) {
+	found = true
+	switch ty := ctxPtr.(type) {
+	case *error:
+		if c.err == nil {
+			return false
+		}
+		*ty = c.err
+	case *http.ResponseWriter:
+		*ty = c.ResponseWriter
+	default:
+		panic(&wrap.ErrUnsupportedContextGetter{ctxPtr})
+	}
+	return
+}
+
+func (c *basicContext) SetContext(ctxPtr interface{}) {
+	switch ty := ctxPtr.(type) {
+	case *error:
+		c.err = *ty
+	default:
+		panic(&wrap.ErrUnsupportedContextSetter{ctxPtr})
+	}
+}
+
+func (c basicContext) Wrap(next http.Handler) http.Handler {
+	var f http.HandlerFunc
+	f = func(rw http.ResponseWriter, req *http.Request) {
+		next.ServeHTTP(&basicContext{ResponseWriter: rw}, req)
+	}
+	return f
+}
---
+++ stack/stack.go
@@ -0,0 +1,54 @@
+/*
+Package stack provides shortcuts for applications that need just one top level stack
+*/
+package stack
+
+import (
+	"net/http"
+
+	"github.com/go-on/wrap"
+)
+
+var context wrap.ContextInjecter
+
+var wrapper []wrap.Wrapper
+
+// New checks inject for validity and sets it as the global top level context
+func New(inject wrap.ContextInjecter) {
+	if context != nil {
+		panic("context already defined")
+	}
+	if inject == nil {
+		context = &basicContext{}
+	} else {
+		wrap.ValidateContextInjecter(inject)
+		context = inject
+	}
+	wrapper = []wrap.Wrapper{context}
+}
+
+// Check may be used in to compose Wrapper with inner middleware stacks like in
+// It checks if the wrappers context requirements are fulfilled by the top level context
+// stack.Use( wrap.New(stack.Check(wrapper1), stack.Check(wrapper2)) )
+func Check(w wrap.Wrapper) wrap.Wrapper {
+	if context == nil {
+		panic("no context defined, use New")
+	}
+	wrap.ValidateWrapperContexts(context, w)
+	return w
+}
+
+// Use adds the given wrappers to the middleware stack after checking if their context
+// requirements are fulfilled by the global toplevel context
+func Use(w ...wrap.Wrapper) {
+	if context == nil {
+		panic("no context defined, use New")
+	}
+	wrap.ValidateWrapperContexts(context, w...)
+	wrapper = append(wrapper, w...)
+}
+
+// Handler composes the stack and returns a handler for it
+func Handler() http.Handler {
+	return wrap.New(wrapper...)
+}
